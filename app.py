16e25f2da88efd9b3ffb96ffc50a861442b37d62
Rahul Vaghasia <rjvaghasia@gmail.com>
2023-01-31 00:21:28 -0500

    Initial Commit

---
+++ app.py
@@ -0,0 +1,40 @@
+import openai
+import requests
+from flask import Flask, request
+
+app = Flask(__name__)
+
+# Initialize OpenAI API key
+openai.api_key = "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"
+
+# Your Telegram Bot API key
+TELEGRAM_BOT_TOKEN = "5970232087:AAHpnfPXJtQIA1k2ZFcntRi5_kLdvlWMahE"
+
+# URL for sending messages to Telegram bot
+TELEGRAM_SEND_MESSAGE_URL = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+
+@app.route("/telegram_webhook", methods=["POST"])
+def telegram_webhook():
+    # Extract the message from the Telegram request
+    message = requests.get_json()["message"]["text"]
+
+    # Use GPT-3 to generate a response to the message
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=f"Reply to: {message}\n\n",
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    ).get("choices")[0].text
+
+    # Send the response back to Telegram
+    requests.post(TELEGRAM_SEND_MESSAGE_URL, json={
+        "chat_id": request.get_json()["message"]["chat"]["id"],
+        "text": response,
+    })
+
+    return "OK"
+
+if __name__ == "__main__":
+    app.run()
